9ce3665434325ce849ca7488d6b5bc6345e87eec
ZeroNatioN <michal.krause01@gmail.com>
2018-03-15 01:44:26 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj imię:");
+            string imie = "Michal";
+            imie = Console.ReadLine();
+            Console.WriteLine("Witaj! "+imie);
+        }
+    }
+}
